0ed6309ae4115278ef4cf81a4feed1f95562c4da
unknown <gabeilko@gmail.com>
2014-03-10 17:47:20 -0400

    hook up to service. needs much cleanup.

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -15,21 +15,31 @@ angular.module('sapience.system').controller('HomeController', ['$scope', '$http
     }).error(function(data, status, headers, config) {
         $scope.status = status;
     });
+
+    $http.get("http://localhost:3000/metrics")
+    .success(function(data, status, headers, config) {
+        $scope.loadMetrics(data);
+    }).error(function(data, status, headers, config) {
+        $scope.status = status;
+    });
 		   
   $scope.spiderChartModel = {};
   $scope.lineChartModel = {};
-  $scope.spiderChartModel.expectedSeries = {name:'Expected',data: [2,2,2,3,2,4,1]};
-  $scope.lineChartModel.expectedSeries = {name:'Expected',data: [90,90,90,90,90,90,90,90,90,90,90,90]};
+  $scope.spiderChartModel.expectedSeries = {name:'Expected',data: []};
+  $scope.lineChartModel.expectedSeries = {name:'Expected',data: []};
   
   $scope.spiderApplicationSeries = [];
-  $scope.lineApplicationSeries = [];
-  
+  $scope.lineApplicationSeries = []; 
+
+
+
+
   $scope.applicationToggled = function(application){
 	application.selected = !application.selected;
 
 	if(application.selected){
-		$scope.spiderApplicationSeries.push({name:application.name, data: $scope.metricData[application.id].spider,color:"#"+((1<<24)*Math.random()|0).toString(16)});		
-		$scope.lineApplicationSeries.push({name:application.name, data: $scope.metricData[application.id].line,color:"#"+((1<<24)*Math.random()|0).toString(16)});		
+		$scope.spiderApplicationSeries.push({name:application.name, data: $scope.metricData[application.name],color:"#"+((1<<24)*Math.random()|0).toString(16)});		
+		$scope.lineApplicationSeries.push({name:application.name, data: $scope.metricData[application.name],color:"#"+((1<<24)*Math.random()|0).toString(16)});		
 	}else{
 		var applicationToPop = $.grep($scope.spiderApplicationSeries, function(e){ return e.name == application.name; });
 		$scope.spiderApplicationSeries.pop(applicationToPop);
@@ -40,7 +50,23 @@ angular.module('sapience.system').controller('HomeController', ['$scope', '$http
   $scope.lineChartModel.applicationSeries = $scope.lineApplicationSeries;
 	$scope.buildSpiderChart($scope.spiderChartModel);
 	$scope.buildLineChart($scope.lineChartModel);
-  }
+  };
+
+  $scope.loadMetrics = function(metrics){
+      $scope.metricData = {};
+      $scope.spiderChartModel.categories = [];
+      $scope.lineChartModel.categories = [];
+      metrics.forEach(function(metric,index,array){
+          $scope.spiderChartModel.categories.push(metric.category.name);
+          $scope.lineChartModel.categories.push(metric.category.name);
+          if(metric.product.name in $scope.metricData){
+            $scope.metricData[metric.product.name] = $scope.metricData[metric.product.name].concat([metric.value]);
+          }else{
+            $scope.metricData[metric.product.name] = [metric.value];
+          }
+          
+        });
+  };
 
   $scope.buildLineChart = function(lineChartModel){
 	new Highcharts.Chart({
@@ -57,8 +83,7 @@ angular.module('sapience.system').controller('HomeController', ['$scope', '$http
                 x: -20
             },
             xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                categories: lineChartModel.categories
             },
             yAxis: {
                 title: {
@@ -97,8 +122,7 @@ angular.module('sapience.system').controller('HomeController', ['$scope', '$http
 	    },
 	    
 	    xAxis: {
-	        categories: ['% Code Coverage','Cyclomatic Complexity','Show Stopper', 
-	                'Critical', 'Medium','Low','Trivial'],
+	        categories: spiderChartModel.categories,
 	        tickmarkPlacement: 'on',
 	        lineWidth: 0
 	    },
