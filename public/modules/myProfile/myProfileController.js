be40f95e32ad6f2ac4878fa0f265a6bbf45a1c3b
unknown <sansarmor@.qait.com>
2014-07-09 15:51:48 +0530

    my profile changes

--- public/modules/myProfile/myProfileController.js
+++ public/modules/myProfile/myProfileController.js
@@ -1,19 +1,17 @@
 'use strict';
 
 angular.module('sapience.system').controller('myProfileController', ['$scope' , '$http', function($scope, $http) {
-	
-	console.log('inside My Profile Controller');
-	
-	$scope.updateUserProfile=function(userSession) {
-		
-		console.log('inside update method '+ userSession.password);
-		
-		$http.put('sapience/users/'+userSession._id,userSession).then(function (data) {
+ 
+ console.log('inside My Profile Controller');
+ 
+ $scope.updateUserProfile=function(userSession) {
+  
+  $http.put('sapience/users/'+userSession._id,userSession).success(function (data) {
 
-			console.log('data updated successfully '+data[0]);
-        	
+   console.log('data updated successfully '+data.firstName);
+         $scope.userSession=data;
         })
         
-	}	
+ } 
     }
 ]);
